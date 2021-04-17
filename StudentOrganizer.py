bbb89b2f0f3e4b89083af2b5c29156399c8b4672
Evan Haut <evanwhaut@gmail.com>
2021-04-17 13:47:49 -0700

    failed on gui but testing

--- StudentOrganizer.py
+++ StudentOrganizer.py
@@ -1,8 +1,12 @@
 import tkinter as tkinter
 
+#creates the 
+def monthGenerator(startDate, numberOfDays):
+    print('hi')
+
 window = tkinter.Tk()
 window.title("Calender")
 window.geometry("1000x800")
-btn = tkinter.Button(window, text = 'This should quit the program', bd = '5', command = window.destroy)
-btn.pack(side = 'right')
+monthGenerator(1, 31)
 window.mainloop()
+
