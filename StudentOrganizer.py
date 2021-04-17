e5e8bc1ef1c09819952a05d14fb9a5a6650968bf
Evan Haut <evanwhaut@gmail.com>
2021-04-17 12:52:14 -0700

    testing windows

--- StudentOrganizer.py
+++ StudentOrganizer.py
@@ -1,1 +1,8 @@
-import tkinter as tk 
+import tkinter as tkinter
+
+window = tkinter.Tk()
+window.title("Calender")
+window.geometry("1000x800")
+btn = tkinter.Button(window, text = 'This should quit the program', bd = '5', command = window.destroy)
+btn.pack(side = 'right')
+window.mainloop()
